20c0ebcd9e793d024203932c5576173fb6279bf1
han8909227 <hbao2016@hotmail.com>
2017-08-28 14:03:56 -0700

    done

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -3,8 +3,9 @@ var app = app || {};
 
 (function(module) {
   const aboutController = {};
-
-  // TODO(DONE): Define a function that hides all main section elements, and then reveals just the #about section:
+  // TODO(DONE): Setup a function that kicks off the fetching and rendering of articles, using the same
+  // code that used to be in index.html.
+  // Also be sure to hide all the main section elements, and reveal the #articles section:
   aboutController.init = function() {
     $('.tab-content').hide();
     $('#about').show();
