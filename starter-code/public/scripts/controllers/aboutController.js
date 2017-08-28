5a515777628ccfe1ff9fb6ad0e35a2741c758fe5
ryanforstie <ryanforstie@gmail.com>
2017-08-28 13:30:04 -0700

    finished first half of todos

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,8 +4,11 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+  // TODO(DONE): Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.init = function() {
+    $('.tab-content').hide();
+    $('#about').show();
+  }
 
   module.aboutController = aboutController;
 })(app);
